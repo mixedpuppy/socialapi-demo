d56890cb71f79e22a1c0d673bcb7284d08a80b88
Shane Carveo <mixedpuppy@gmail.com>
2013-05-21 18:16:47 -0700

    full manifest for test server

--- test/prefs.js
+++ test/prefs.js
@@ -1,4 +1,4 @@
 user_pref("social.activeProviders", "{\"http://localhost:3000\":1}");
 user_pref("social.enabled", true);
-user_pref("social.manifest.tests", "{\"name\":\"DemoSocialService\",\"iconURL\":\"http://localhost:3000/firefox16.png\",\"sidebarURL\":\"http://localhost:3000/sidebar.htm\",\"origin\":\"http://localhost:3000\",\"enabled\":true,\"last_modified\":135101330568}");
+user_pref("social.manifest.tests", "{\"origin\": \"http://localhost:3000\", \"name\": \"Demo Social Service\",\"iconURL\": \"http://localhost:3000/firefox16.png\",\"icon32URL\": \"http://localhost:3000/firefox32.png\",\"icon64URL\": \"http://localhost:3000/firefox64.png\",\"workerURL\": \"http://localhost:3000/worker.js\",\"sidebarURL\": \"http://localhost:3000/sidebar.htm\",\"shareURL\": \"http://localhost:3000/share.html#%{url}\",\"description\": \"A short paragraph about this provider\",\"author\": \"Shane Caraveo, Mozilla\",\"homepageURL\": \"https://github.com/mixedpuppy/socialapi-demo/\",\"version\": \"1.0\"}");
 user_pref("social.provider.current", "http://localhost:3000");
